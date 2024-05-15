ac1c88e90a52c4f341025903c991db1b233bf508
LokiAngelMx <LokiGarciaMx@gmail.com>
2024-05-15 15:51:45 -0600

    Fecha

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+document.getElementById('scrollDownArrow').addEventListener('click', function() {
+    const nextContent = document.getElementById('nosotras');
+    nextContent.scrollIntoView({ behavior: 'smooth' });
+});
+
+// countdown.js
+const countdown = () => {
+    const countDate = new Date('Jul 6, 2024 00:00:00').getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector('#countdown .p-2:nth-child(1)').innerText = `${textDay} \n días`;
+    document.querySelector('#countdown .p-2:nth-child(2)').innerText = `${textHour} \n horas`;
+    document.querySelector('#countdown .p-2:nth-child(3)').innerText = `${textMinute} \n minutos`;
+    document.querySelector('#countdown .p-2:nth-child(4)').innerText = `${textSecond} \n segundos`;
+};
+
+setInterval(countdown, 1000);
